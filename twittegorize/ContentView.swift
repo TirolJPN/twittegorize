930d5ab798e34273d6f034b4bd3d6613bd740210
Tirol_JPN <Tirol.JPN@gmail.com>
2019-10-15 06:53:01 +0900

    first commit

---
+++ twittegorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  twittegorize
+//
+//  Created by 原口公輔 on 2019/10/15.
+//  Copyright © 2019 Kosuke haraguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
