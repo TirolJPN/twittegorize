33896c25cc714f6924aa589e783d754090194d2a
Tirol_JPN <Tirol.JPN@gmail.com>
2019-10-21 17:50:48 +0900

    add Model files

---
+++ twittegorize/Model/Tweet.swift
@@ -0,0 +1,17 @@
+//
+//  Tweet.swift
+//  twittegorize
+//
+//  Created by 原口公輔 on 2019/10/21.
+//  Copyright © 2019 Kosuke haraguchi. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let id: String
+    
+    let text: String
+    
+    let user: User
+}
