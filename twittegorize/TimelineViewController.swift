6139164deebf12f8d3739eb57bc6672c7c73acb5
Tirol_JPN <Tirol.JPN@gmail.com>
2019-10-22 18:22:08 +0900

    add new components to stroyborad and a config swift file

---
+++ twittegorize/TimelineViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TimelineViewController.swift
+//  twittegorize
+//
+//  Created by 原口公輔 on 2019/10/22.
+//  Copyright © 2019 Kosuke haraguchi. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
