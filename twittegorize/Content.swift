b580a677ae31a4c1d0cf6cdf12ab88080e78ee08
Tirol_JPN <Tirol.JPN@gmail.com>
2019-11-01 16:11:24 +0900

    add Content.swift which operate overall view and tab view to display category page

---
+++ twittegorize/Content.swift
@@ -0,0 +1,30 @@
+//
+//  Content.swift
+//  twittegorize
+//
+//  Created by 原口公輔 on 2019/11/01.
+//  Copyright © 2019 Kosuke haraguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct Content: View {
+    var body: some View {
+        TabView {
+            TweetList()
+                .tabItem {
+                    Text("Tweet List")
+                }
+            Text("Category")
+                .tabItem {
+                    Text("Category")
+                }
+        }
+    }
+}
+
+struct Content_Previews: PreviewProvider {
+    static var previews: some View {
+        Content()
+    }
+}
